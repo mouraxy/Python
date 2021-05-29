d10e209d41306cb1f6995c07fcb895889debcfc2
Felipe Paskevicius <xmourax@hotmail.com>
2021-05-29 15:36:50 -0300

    UpdatePython
    Inserir e utilização de comentários, variáveis. além da função (print).

---
+++ Curso Python/Aula2.py
@@ -0,0 +1,9 @@
+#Para incluir comentários de uma linha utiliza-se #
+
+#Imprimindo uma mensagem aleatória (ou não)
+print("Remember Reach!")
+
+"""
+Para incluir comentários de mais de   
+uma linha utiliza-se três aspas na abertura e no fechamento
+"""
