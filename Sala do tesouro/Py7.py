a6dff92b28645c29df73e49b3562665360b6ec5e
Felipe Paskevicius <xmourax@hotmail.com>
2021-09-25 19:16:41 -0300

    Comparações

---
+++ Sala do tesouro/Py7.py
@@ -0,0 +1,22 @@
+#Inequações (comparações)
+
+Idade = 20
+DobroIdade = Idade*2
+
+#Igualdade == 
+Idade == Idade
+
+#Maior >
+DobroIdade > Idade
+
+#Menor <
+Idade < DobroIdade
+
+#Maior igual >=
+Idade >= Idade
+
+#Menor igual <=
+Idade <= Idade
+
+#Diferente !=
+Idade != DobroIdade
