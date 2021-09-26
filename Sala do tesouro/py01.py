080907cd38ae3e826041d8ce877e1ae997e0c7b5
Felipe Paskevicius <xmourax@hotmail.com>
2021-09-25 21:23:38 -0300

    Comentários

---
+++ Sala do tesouro/py01.py
@@ -0,0 +1,10 @@
+#Comentários
+#Para incluir comentários em uma linha utiliza-se #
+
+#Imprimindo uma mensagem aleatória (ou não)
+print("Remember Reach!")
+
+"""
+Para incluir comentários de mais de   
+uma linha utiliza-se três aspas na abertura e no fechamento
+"""
