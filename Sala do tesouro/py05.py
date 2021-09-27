79f2b34b56a5fbd183bc25d1d2fa6a31b7f3bae0
Felipe Paskevicius <xmourax@hotmail.com>
2021-09-26 23:25:43 -0300

    Formatação
    Maiores detalhamentos sobre as funções "print" e "input" no Pyhton.

--- Sala do tesouro/py05.py
+++ Sala do tesouro/py05.py
@@ -1,15 +1,17 @@
-#Atalhos na função print (maior legibilidade)
+#Atalhos e formatações da função print e input (maior legibilidade)
 Dia = 18
 Mes = 7
 Ano = 2001
-
-print("Data de nascimento do Felipe: ", Dia,"/", Mes,"/", Ano)
-
-#Mesmo resultado, utilizando-se agora o % na função print (formato da variável)
-print("Data de nascimento do Felipe: %i/%i/%i" %(Dia, Mes, Ano))
 
 #Para inteiros --> %i ou %d
 #Para floats (números decimais) --> %f
 #Para strings (textos) --> %s
 
+#print
+print("Data de nascimento do Felipe: ", Dia,"/", Mes,"/", Ano)
 
+#Mesmo resultado, utilizando-se agora o % na função print (formato da variável)
+print("Data de nascimento do Felipe: %i/%i/%i" %(Dia, Mes, Ano))
+
+#input 
+input("De acordo com o algoritmo, você faz aniversário no dia %i. Pode confirmar o valor? " %Dia)
