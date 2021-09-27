54813e37195c95ce8ae5bf934ceee1c45a04abfe
Felipe Paskevicius <xmourax@hotmail.com>
2021-09-26 23:16:49 -0300

    Listas

--- Sala do tesouro/py20.py
+++ Sala do tesouro/py20.py
@@ -1,10 +1,10 @@
 #Listas: função len e método count
 
-#Função len calcula a quantidade de elementos em uma lista - (nome da lista)
+#Função len calcula a quantidade de elementos em uma lista - len(nome da lista)
 lista = [1, 2, 3, 3, 3, 3, 3]
 len(lista)
 
-#Método count define quantas vezes um determinado valor aparece em uma lista - (valor a ser procurado)
+#Método count define quantas vezes um determinado valor aparece em uma lista - nome da lista.count(valor a ser procurado)
 lista.count(3)
 
 
