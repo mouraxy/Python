f51adc0e5c083474302bca2d28a525d420742c42
Felipe Paskevicius <xmourax@hotmail.com>
2021-10-09 15:35:31 -0300

    Strings

---
+++ Sala do tesouro/py28.py
@@ -0,0 +1,21 @@
+#Strings
+#São conjuntos/formatos textuais
+
+#São criadas através de aspas:
+"Olá, isso é uma string"
+'Olá, isso é uma string'
+"""Olá, 
+isso é uma string"""
+
+#Podem ser armazenadas em variáveis:
+Guardar = "string"
+
+#Concatenar: somar uma string com outra
+"Guardando " + Guardar
+"Juntando " "uma frase"
+
+#Pode-se multiplicar uma string:
+"Multiplicando " * 5
+
+#Convertendo determinados valores para string's - str(dados):
+str(0)
