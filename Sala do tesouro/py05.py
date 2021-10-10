66230c976686a9660a43b857d6e07a69b856e471
Felipe Paskevicius <xmourax@hotmail.com>
2021-10-10 17:45:08 -0300

    Formatos

--- Sala do tesouro/py05.py
+++ Sala do tesouro/py05.py
@@ -1,43 +1,26 @@
-#Atalhos e formatações da função print e input (maior legibilidade)
+#Formatações da função print e input
 Dia = 18
 Mes = 7
 Ano = 2001
 Decimal = 3.1415
 
-#Para inteiros --> %i ou %d
-#Para floats (números decimais) --> %f
-#Para strings (textos) --> %s
+#Função print normal:
+print("\nData de nascimento do Felipe: ", Dia,"/", Mes,"/", Ano)
 
-#Função print e seus detalhamentos:
-#print normal
-print("Data de nascimento do Felipe: ", Dia,"/", Mes,"/", Ano)
+#Utilizando-se agora dos {} e o do f antes das aspas:
+print(f"Data de nascimento do Felipe: {Dia}/{Mes}/{Ano}")
 
-#Mesmo resultado, utilizando-se agora o % na função print (formato da variável)
-print("Data de nascimento do Felipe: %i/%i/%i" %(Dia, Mes, Ano))
+#Definindo as casas decimais com o {:.númeroDeCasasDecimaisf}
+print(f"O valor de PI é: {Decimal:.2f}")
 
-#Mesmo resultado, porém utilizando agora as {} e o .format(variáveis)
-print("Data de nascimento do Felipe: {}/{}/{}" .format(Dia, Mes, Ano))
-
-#O %.(valor)f define o número de casas decimais depois da vírgula
-print("\nSua média final é: %.2f" %Decimal)
-
-#Definição das casas decimais, agora com o {:.númeroDeCasasDecimaisf}
-print("\nSua média final é: {:.2f}" .format(Decimal))
-
-#Para evitar quebra de linha, utiliza-se (, end = " ")
+#Para evitar quebras de linha utiliza-se (end = "separador"):
 print("Evitando quebra de linha", end = " ")
+print("com o o end!\n")
 
+#Função input com os {} e o f antes das aspas:
+input(f"De acordo com o algoritmo, você faz aniversário no dia {Dia}. Pode confirmar o valor? ")
 
-#Função input e seus detalhamentos:
-#input utilizando o %
-input("De acordo com o algoritmo, você faz aniversário no dia %i. Pode confirmar o valor? " %Dia)
-
-#input utilizando as {} e o .format(variável)
-input("De acordo com o algoritmo, você faz aniversário no dia {}. Pode confirmar o valor? " .format(Dia))
-
-
-#Métodos: 
-#type(variável): retorna o tipo do dado
+#Método type(variável): retorna o tipo do dado
 type(Dia)
 
-#variável.is____(): tem várias opções e retorna diversas informações de testagem ("é alguma coisa?")
+#Pesquisar sobre os métodos is() --> É alguma coisa??
